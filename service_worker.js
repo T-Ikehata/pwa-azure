2c71ce105d2f73f800d9be06c7834466f51448f0
T-Ikehata <81730900+T-Ikehata@users.noreply.github.com>
2021-11-08 11:35:57 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,8 +1,38 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.cacheFirst()
-);
+
+const OFFLINE_PAGE = '/pwa-azure/index.html';
+workbox.precaching.precacheAndRoute([
+  OFFLINE_PAGE,
+]);
+
+workbox.routing.setCatchHandler(({ event }) => {
+  switch (event.request.destination) {
+    case 'document':
+      return caches.match(OFFLINE_PAGE);
+    default:
+      return Response.error();
+  }
+});
+
+workbox.routing.registerRoute(({ url, request }) => {
+  const hostnames = [
+    // キャッシュを許可するドメイン名のリスト
+    't-ikehata.github.io',
+  ];
+  const types = [
+    // キャッシュを許可するリソースの種別
+    'font',
+    'script',
+    'style',
+    'image',
+  ];
+  return (
+    hostnames.some(hostname => url.hostname === hostname) &&
+    types.some(type => request.destination === type)
+  );
+}, new workbox.strategies.StaleWhileRevalidate());
+
+workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
 
 // push 通知の待ち受け
 self.addEventListener('push', function(event) {
