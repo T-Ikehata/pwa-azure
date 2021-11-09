a79d01974f3d24eb944ff396044eb161193ad9fd
T-Ikehata <81730900+T-Ikehata@users.noreply.github.com>
2021-11-09 16:41:13 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -4,7 +4,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox
 // オフラインページ指定
 const OFFLINE_PAGE = '/pwa-azure/';
 workbox.precaching.precacheAndRoute([
-  { url: OFFLINE_PAGE, revision: '35' },
+  { url: OFFLINE_PAGE, revision: '36' },
 ]);
 
 // オフラインページへのキャッシュ適用
